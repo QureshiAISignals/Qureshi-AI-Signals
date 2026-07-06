72b3a29f614ad1223e6822a8745af0f160962332
QureshiAISignals <qureshiaisignals@gmail.com>
2026-07-06 04:44:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,49 @@
 function startAI(){
 
-let result = generateSignal();
+let pair = document.getElementById("pair").value;
 
-document.getElementById("signal").innerHTML = result.signal;
+let result = {
+pair: pair,
+signal: "🟡 WAIT",
+confidence: 50,
+trend: "Sideways",
+reason: "Waiting for confirmation"
+};
 
-document.getElementById("confidence").innerHTML =
-"Confidence: " + result.confidence + "%";
+if(pair=="EUR/USD"){
+
+result.signal="🟢 BUY";
+result.confidence=86;
+result.trend="Bullish 📈";
+result.reason="EMA + RSI + MACD";
+
+document.getElementById("ema").innerHTML="EMA : +20";
+document.getElementById("rsi").innerHTML="RSI : +15";
+document.getElementById("macd").innerHTML="MACD : +20";
+document.getElementById("bollinger").innerHTML="Bollinger : +10";
+document.getElementById("stochastic").innerHTML="Stochastic : +10";
+document.getElementById("cci").innerHTML="CCI : +5";
+document.getElementById("candlestick").innerHTML="Candlestick : +6";
 
 }
 
-window.onload = startAI;
+else if(pair=="GBP/USD"){
+
+result.signal="🔴 SELL";
+result.confidence=82;
+result.trend="Bearish 📉";
+result.reason="RSI + CCI + Bollinger";
+
+document.getElementById("ema").innerHTML="EMA : -15";
+document.getElementById("rsi").innerHTML="RSI : -20";
+document.getElementById("macd").innerHTML="MACD : -15";
+document.getElementById("bollinger").innerHTML="Bollinger : -10";
+document.getElementById("stochastic").innerHTML="Stochastic : -10";
+document.getElementById("cci").innerHTML="CCI : -10";
+document.getElementById("candlestick").innerHTML="Candlestick : -2";
+
+}
+
+updateDashboard(result);
+
+}

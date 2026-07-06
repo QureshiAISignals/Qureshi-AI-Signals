f247b20871a8b8067d1d7774b05bafacc97ad347
QureshiAISignals <qureshiaisignals@gmail.com>
2026-07-06 03:58:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 
+function analyze(){
+
+let pair=document.getElementById("pair").value;
+
+let signal="WAIT";
+let confidence=65;
+let trend="Sideways";
+let reason="No Clear Setup";
+
+if(pair=="EUR/USD"){
+signal="🟢 BUY";
+confidence=82;
+trend="Bullish 📈";
+reason="EMA + RSI + Bollinger";
+}
+
+if(pair=="GBP/USD"){
+signal="🔴 SELL";
+confidence=79;
+trend="Bearish 📉";
+reason="RSI + CCI + Stochastic";
+}
+
+document.getElementById("signal").innerHTML=signal;
+document.getElementById("confidence").innerHTML="Confidence: "+confidence+"%";
+document.getElementById("pairName").innerHTML="Selected Pair: "+pair;
+document.getElementById("trend").innerHTML="Trend: "+trend;
+document.getElementById("reason").innerHTML="Reason: "+reason;
+
+}

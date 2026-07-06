db0f1982d7dc606b11a62a7e27b7a0825827ab1e
QureshiAISignals <qureshiaisignals@gmail.com>
2026-07-06 04:33:03 +0300

    Create engine.js

---
+++ engine.js
@@ -0,0 +1,43 @@
+const ENGINE = {
+
+name: "Qureshi AI Engine",
+
+version: "1.0",
+
+indicators: [
+
+"EMA",
+
+"RSI",
+
+"MACD",
+
+"Bollinger Bands",
+
+"Stochastic",
+
+"CCI",
+
+"ATR",
+
+"ADX",
+
+"SuperTrend",
+
+"Williams %R",
+
+"MFI",
+
+"VWAP",
+
+"Ichimoku",
+
+"Support & Resistance",
+
+"Candlestick"
+
+],
+
+status: "INITIALIZING"
+
+};

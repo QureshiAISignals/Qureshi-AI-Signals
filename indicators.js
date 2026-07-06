9ef3992d3fbe7818d7c7f38279a9d53cef480cfb
QureshiAISignals <qureshiaisignals@gmail.com>
2026-07-06 04:12:21 +0300

    Create indicators.js

---
+++ indicators.js
@@ -0,0 +1,21 @@
+function getIndicators(pair){
+
+return {
+
+ema: Math.random() > 0.5,
+
+rsi: Math.random() > 0.5,
+
+macd: Math.random() > 0.5,
+
+bollinger: Math.random() > 0.5,
+
+stochastic: Math.random() > 0.5,
+
+cci: Math.random() > 0.5,
+
+candlestick: Math.random() > 0.5
+
+};
+
+}
